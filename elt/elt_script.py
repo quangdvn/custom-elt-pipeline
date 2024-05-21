f3b6742a280df18583347dff56a6a8d2cde545e8
Nhat Quang <ghitacluber@gmail.com>
2024-05-21 11:34:43 +0900

    fix text

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -4,7 +4,7 @@ import time
 
 
 def wait_for_portgres(host, max_retries=5, delay_seconds=5):
-  """Wait for PostgreSQL to become available."""
+  """Wait for PostgreSQL to become available"""
   retries = 0
   while retries < max_retries:
     try:
